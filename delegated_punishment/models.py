428121e02844b5f9f12272d1283694a8a08871f5
Wesley Smith <yelsew414@gmail.com>
2020-03-05 13:21:51 -0700

    changed some small styling things and updated the game params

--- delegated_punishment/models.py
+++ delegated_punishment/models.py
@@ -28,17 +28,18 @@ class Constants(BaseConstants):
     start_balance = 0
 
     # these variables will be subject to change the most
-    civilian_fine_amount = 540
-    civilian_steal_rate = 6  # S: amount of grain stolen per second (CONSTANT ACROSS GROUPS AND PERIODS)
+    civilian_fine_amount = 288
+    civilian_steal_rate = 19  # S: amount of grain stolen per second (CONSTANT ACROSS GROUPS AND PERIODS)
 
-    officer_review_probability = .1  # THETA: chance that an intersection result will be reviewed
-    officer_reprimand_amount = 100  # P punishment for officer if innocent civilian is punished
+    officer_review_probability = .2  # THETA: chance that an intersection result will be reviewed
+    officer_reprimand_amount = 50  # P punishment for officer if innocent civilian is punished
 
-    civilian_incomes_low = [3, 5, 8, 10]
-    civilian_incomes_high = [2, 3, 4, 15]
+    civilian_incomes_low = [32, 35, 30, 50]
+    civilian_incomes_high = [18, 23, 32, 84]
     officer_incomes = [0, 5, 10, 15]
 
-    defend_token_size = 36  # this is the size of the tokens that players with role of officer drag around
+    # also change in officer.css
+    defend_token_size = 56  # this is the size of the tokens that players with role of officer drag around
     civilian_map_size = 240
 
 
